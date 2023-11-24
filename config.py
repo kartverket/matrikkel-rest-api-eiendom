600058965bbc84d7f2855a5409c62979cdb977b1
chgr007 <71885781+chgr007@users.noreply.github.com>
2023-11-24 08:57:10 +0100

    locale støttet av mac/linux

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ password = os.environ.get('PG_PASS_EIENDOM')
 dbc = {'database': database, 'user': user, 'port': port, 'host': host, 'password': password}
 
 set_json_as_ascii = False
-locale_choice = 'nn_NO.utf8'
+locale_choice = 'no_NO.UTF-8'
 logging_level = os.environ.get('EIENDOM_API_LOG_LEVEL', 'ERROR')
 sort_json_keys = False
 
