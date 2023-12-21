c48b6539459dfbbcaa52ae68ce6af56ada4ae8c6
anderssonw <wandersson.98@gmail.com>
2023-12-21 12:40:47 +0100

    Legg til CORS Headers
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import locale
 import logging
 
 from flask import Flask
+from flask_cors import CORS
 
 import config as cf
 
 app = Flask(__name__)
+CORS(app)
 
 locale.setlocale(locale.LC_ALL, cf.locale_choice)  # to sort æøå correctly
 app.config['JSON_AS_ASCII'] = cf.set_json_as_ascii
